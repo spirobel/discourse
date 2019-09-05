0fe19228917147f86c6012b9a15e21df28c848e2
Spiro Spirobel <spirobel@protonmail.com>
2019-09-06 00:23:54 +0200

    unify with modelgenerator PLUGINNAME_migrationmae -> Pluginname

--- lib/generators/plugin/migration_generator.rb
+++ lib/generators/plugin/migration_generator.rb
@@ -3,14 +3,14 @@
 require "rails/generators/active_record/migration/migration_generator"
 
 class Plugin::MigrationGenerator < ActiveRecord::Generators::MigrationGenerator
-  desc "Expecting migration NAME to start with name of plugin: PLUGINFOLDER_migration_name"
+  desc "Expecting NAME to start with name of plugin: PluginnameMigrationname"
   source_root "#{Gem.loaded_specs["activerecord"].full_gem_path}/lib/rails/generators/active_record/migration/templates"
   private
 
   def db_migrate_path
-    plugin_folder = 'plugins/' + name.split("_").first
+    plugin_folder = 'plugins/' + name.underscore.split("_").first
     unless File.directory?(Rails.root.join(plugin_folder))
-     print "#{plugin_folder} does not exit. Expecting migration NAME to start with name of plugin: PLUGINFOLDER_migration_name"
+     print "#{plugin_folder} does not exit. Expecting NAME to start with name of plugin: PluginnameMigrationname"
      exit
     end
     plugin_folder + "/db/migrate"
